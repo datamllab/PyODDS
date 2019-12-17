cc57feb790d29ad8f4aaae7fa8918a91791dd0b6
Praveen Venugopal <razorvine@tamu.edu>
2019-12-17 10:50:22 -0600

    test commit

--- test/demo.py
+++ test/demo.py
@@ -64,6 +64,7 @@ if __name__ == '__main__':
     print('Load data successful')
 
     #algorithm
+    # TODO: Add CASH here
 
     clf = algorithm_selection(args.algorithm,random_state=rng,contamination=args.contamination)
     print('Start processing:')
